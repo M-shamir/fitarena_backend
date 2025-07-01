fc2cded8411ea3f63b08150f530cc9a51876af7a
M-shamir <mshamir732gmail.com>
2025-07-01 20:00:48 +0530

    fix(auth):refresh token cookie issue in  facebook auth

--- account_app/views.py
+++ account_app/views.py
@@ -282,6 +282,14 @@ class FacebookLogin(APIView):
                     samesite='Lax',
                     secure=False
                 )
+                response.set_cookie(
+                    key="refresh_token",
+                    value=str(refresh),
+                    httponly=True,
+                    expires=datetime.utcnow() + timedelta(days=settings.REFRESH_TOKEN_EXPIRY_DAYS),
+                    samesite='Lax',
+                    secure=False
+                )
                 
                 return response
 
