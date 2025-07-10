480a180907974bd22061e04b5ca2029ba04efb0e
M-shamir <mshamir732gmail.com>
2025-07-10 23:59:40 +0530

    fix:facebook

--- user/urls.py
+++ user/urls.py
@@ -5,7 +5,7 @@ from account_app.views import GoogleLogin,FacebookLogin
 urlpatterns =  [
     path('auth/signup',SignUpView.as_view(),name='signup'),
     path('auth/google/', GoogleLogin.as_view(), name='google-login'),
-    # path('auth/facebook/', FacebookLogin.as_view(), name='facebook-login'),
+    path('auth/facebook/', FacebookLogin.as_view(), name='facebook-login'),
     path('auth/verifyotp',UserVerifyOtpView.as_view(),name='verifyotp'),
     path('auth/resendotp',UserResendOtpView.as_view(),name='resendotp'),
     path('auth/login',LoginView.as_view(),name='login'),
